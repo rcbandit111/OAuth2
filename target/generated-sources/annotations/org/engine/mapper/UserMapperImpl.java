0ac86add2aa809a43ce953e4ecbc87d633e16ce1
plte1219 <plamen.terziev@netcracker.com>
2020-07-12 15:12:04 +0300

    Updated code

--- target/generated-sources/annotations/org/engine/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/org/engine/mapper/UserMapperImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-07-12T00:59:45+0300",
+    date = "2020-07-12T15:10:28+0300",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 14 (Oracle Corporation)"
 )
 @Component
