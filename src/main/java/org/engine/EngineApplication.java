9446002df0d95e8b58dab5a27263c12a15937635
plte1219 <plamen.terziev@netcracker.com>
2020-07-15 16:42:17 +0300

    Updated code

--- src/main/java/org/engine/EngineApplication.java
+++ src/main/java/org/engine/EngineApplication.java
@@ -2,9 +2,9 @@ package org.engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2WebMvc
+@EnableSwagger2
 @SpringBootApplication(scanBasePackages = { "org.engine.*", "org.engine.production.service", "org.engine.warehouse.service" })
 public class EngineApplication {
 
